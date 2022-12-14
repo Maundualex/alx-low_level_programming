e85e40e9c8d46942542d66b331230f8a5f6215ae
Maundualex <maundualex@gmail.com>
2022-12-14 06:30:29 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @z: THe int to be operated upon
+ *
+ * Return: returns an unsigneed int value to calling function
+ */
+
+int _abs(int z)
+{
+	if (z > 0)
+	{
+		return (z);
+	}
+
+	else
+	{
+		z *= -1;
+		return (n);
+	}
+}
