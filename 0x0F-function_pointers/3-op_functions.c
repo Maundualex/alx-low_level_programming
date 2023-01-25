466cfde627a5dba7c831ff423b5c677054acce2b
Maundualex <maundualex@gmail.com>
2023-01-25 04:56:51 -0800

    task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,12 +1,11 @@
 /**
- * op_add - Returns the sum of two numbers
+ * op_add - Returns the sum of @ and @b
  *
- * @a: The first number
- * @b: The second number
+ * @a: input 1
+ * @b: input 2
  *
  * Return: The sum of @a and @b
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
@@ -14,10 +13,10 @@ int op_add(int a, int b)
 
 
 /**
- * op_sub - Returns the difference of two numbers
+ * op_sub - Returns the difference of @a and @b
  * 
- * @a: The first number
- * @b: The second number
+ * @a: input 1
+ * @b: input 2
  *
  * Return: The difference of @a and @b
  */
@@ -28,10 +27,10 @@ int op_sub(int a, int b)
 
 
 /**
- * op_mul - Returns the product of two numbers
+ * op_mul - Returns the product of @a and @b
  *
- * @a: The first number
- * @b: The second number
+ * @a: input 1
+ * @b: input 2
  *
  * Return: The product of @a and @b
  */
@@ -42,12 +41,12 @@ int op_mul(int a, int b)
 
 
 /**
- * op_div - Returns the division of two numbers
+ * op_div - Returns the division of @a and @b
  *
- * @a: The first number
- * @b: The second number
+ * @a: input 1
+ * @b: input 2
  *
- * Return: The quotient of @a and @b
+ * Return: The division of @a and @b
  */
 int op_div(int a, int b)
 {
@@ -56,10 +55,10 @@ int op_div(int a, int b)
 
 
 /**
- * op_mod - Returns the remainder of the division of two numbers
+ * op_mod - Returns the remainder of the division of @a and @b
  *
- * @a: The first number
- * @b: The second number
+ * @a: input 1
+ * @b: input 2
  *
  * Return: The remainder of the division of @a by @b
  */
