00969861b8a0212ef43d10ab46593ab76394e0ab
Maundualex <maundualex@gmail.com>
2023-01-25 05:50:41 -0800

    task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -5,7 +5,7 @@
  * @b: input 2
  *
  * Return: sum of @a and @b
- */
+*/
 
 int op_add(int a, int b)
 {
@@ -20,7 +20,7 @@ int op_add(int a, int b)
  * @b: input 2
  *
  * Return: difference of @a and @b
- */
+*/
 int op_sub(int a, int b)
 {
 	return (a - b);
@@ -34,7 +34,7 @@ int op_sub(int a, int b)
  * @b: input 2
  *
  * Return: product of @a and @b
- */
+*/
 int op_mul(int a, int b)
 {
 	return (a * b);
@@ -48,7 +48,7 @@ int op_mul(int a, int b)
  * @b: input 2
  *
  * Return: results of the division @a and @b
- */
+*/
 int op_div(int a, int b)
 {
 	return (a / b);
@@ -62,7 +62,7 @@ int op_div(int a, int b)
  * @b: input 2
  *
  * Return: the remainder
- */
+*/
 int op_mod(int a, int b)
 {
 	return (a % b);
