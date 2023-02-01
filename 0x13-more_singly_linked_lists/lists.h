a9cc2fb1c3c306d61ddbb63caf3d3d1aa46c971e
Maundualex <maundualex@gmail.com>
2023-02-01 12:38:48 -0800

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,12 +10,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * for Holberton project
  */
 typedef struct listint_s
 {
 	int n;
-	struct listin_s *next;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
